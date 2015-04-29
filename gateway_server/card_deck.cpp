18578bc3ceb21d89f6a69756f763664830f3a5c4
Christophe <christophe_schmol@yahoo.de>
2015-04-30 00:26:22 +0200

    First work on c++ project

---
+++ gateway_server/card_deck.cpp
@@ -0,0 +1,74 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  card_deck.cpp
+ *
+ *    Description:  Card deck for the poker game
+ *
+ *        Version:  1.0
+ *        Created:  2015-04-30 00:03:53
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Christophe Schmol (cs), christophe_schmol@yahoo.de
+ *        Company:  RWTH Aachen University
+ *
+ * =====================================================================================
+ */
+
+#include	<iostream>
+
+#include	<algorithm>
+#include	<vector>
+#include	<ctime>
+#include	<cstdlib>
+
+typedef struct card {
+	char suit;
+	int number;
+} poker_card;
+
+int myrandom (int i) { return std::rand()%i;}
+
+/*
+ * =====================================================================================
+ *        Class:  Card_deck
+ *  Description:  
+ * =====================================================================================
+ */
+class Card_deck
+{
+	public:
+		/* ====================  LIFECYCLE     ======================================= */
+		Card_deck () {                           /* constructor */
+			cout << "Card_deck created" << endl;
+			reset();
+			std::srand(unsigned(time(0)));
+			return;
+		}
+
+		/* ====================  ACCESSORS     ======================================= */
+
+		/* ====================  MUTATORS      ======================================= */
+
+		/* ====================  OPERATORS     ======================================= */
+		poker_card draw_card() {
+			poker_card res = card_deck.back();  /* get the last element */
+			card_deck.pop_back();               /* delete it from the vector */
+			return res;                         /* and return a copy of it */
+			
+		}
+		void shuffle() {
+			std::random_shuffle(poker_deck.begin(), poker_deck.end()); /* shuffle the deck */
+			return;
+		}
+		void reset();
+
+		/* ====================  DATA MEMBERS  ======================================= */
+	protected:
+
+	private:
+		std::vector<poker_card> card_deck;
+
+}; /* -----  end of class Card_deck  ----- */
+
---
+++ gateway_server/server.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015-04-30 00:01:51
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Christophe Schmol (cs), christophe_schmol@yahoo.de
+ *        Company:  RWTH Aachen University
+ *
+ * =====================================================================================
+ */
+
+#include	<iostream>
+
+
+	int
+main ( int argc, char *argv[] )
+{
+	return 0;
+}				/* ----------  end of function main  ---------- */
