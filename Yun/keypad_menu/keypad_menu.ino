def54c8433cfad213cab2cd6647b909901ef6293
junketsu <kingselim1@gmail.com>
2015-06-10 16:21:11 +0200

    further implementations

--- Yun/keypad_menu/keypad_menu.ino
+++ Yun/keypad_menu/keypad_menu.ino
@@ -8,7 +8,7 @@ YunClient client;
 
 int readPin = A1;
 int potiValue = 0;
-int helper;
+char helper[5] = "call";
  
 // Initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -18,10 +18,11 @@ int currentMenuItem = 0;
 int lastState = 0;
 
 //ip adress of the server 
-byte ip[] = {192, 168, 240, 128};
+byte ip[] = {192, 168, 240, 215};
 
 //values from server
-int bet, chips;
+int bet, chips, highbet, action;
+float odds;
 
 
 
@@ -50,8 +51,8 @@ void setup() {
 void loop() 
 {
   //Call the main menu.
-  mainMenu();
   displayChipsAndBet();
+  mainMenu();
   delay(100);
 }
 
@@ -64,7 +65,7 @@ void mainMenu() {
   //Refresh the button pressed.
   int x = analogRead (A0);
   //Set the Row 0, Col 0 position.
-  lcd.setCursor(0,0);
+  lcd.setCursor(8,1);
  
   //Check analog values from LCD Keypad Shield
   if (x < 100) 
@@ -128,19 +129,26 @@ void mainMenu() {
       }
       
       //if Select
-      else if (state == 3)
+      else if (state == 3 && action == 1)
       { 
         switch (currentMenuItem)
         {
           case 0:  //->Check
+            client.print("check\n")
+            action = 0;
             break;
           case 1:  //->Call
+            client.print("call\n")
+            action = 0;
             break;
           case 2:  //->Rise
-            helper = potiValue * chips;
-            client.write(chips);
+            //helper = "call";
+            client.print("raise \n");
+            action = 0;
             break;
           case 3:  //->Fold
+            client.print("fold\n");
+            action = 0;
             break;
           
         }
@@ -159,7 +167,10 @@ void mainMenu() {
 //Display Menu Option based on Index.
 void displayMenu(int x) 
 {
-     switch (x) {
+     
+  if(action == 1)
+  {
+      switch (x) {
       case 0:
         lcd.print ("-> Check        ");
         break;
@@ -167,46 +178,100 @@ void displayMenu(int x)
         lcd.print ("-> Call        ");
         break;
       case 2:
-        lcd.print ("-> Rise        ");
+        lcd.print ("-> Raise        ");
         break;
       case 3:
         lcd.print ("-> Fold        ");
         break;
     }
+  }
+  
+  else
+  {
+    lcd.print("wait        ");
+  }
 }
 
 
 //display bet and chips
 void displayChipsAndBet()
 {
-  lcd.setCursor(0,1);
+  /*lcd.setCursor(0,1);
   //lcd.print("hallo");
   if(client.available())
   {
     String wort = client.readStringUntil('/');
-    Console.println(wort);
-    if (wort == "bet")
-    {
+    //Console.println(wort);
+    //if (wort == "bet")
+    //{
       bet = client.parseInt();
-      Console.println(bet);
+      //Console.println(bet);
       lcd.setCursor(0,1);
       lcd.print("B ");
       lcd.print(bet);
-      client.write(int(bet));
-    }
+      client.write(bet);
+    //}
   
     wort = client.readStringUntil('/');
-    wort = client.readStringUntil('/');
-    Console.println(wort);
+    //wort = client.readStringUntil('/');
+    //Console.println(wort);
     
-    if(wort == "chips")
-    {
+    //if(wort == "chips")
+    //{
       chips = client.parseInt();
       Console.println(chips);
       lcd.setCursor(9,1);
       lcd.print("CP ");
       lcd.print(chips);
-    }
+    //}*/
+  
+  
+  String wort;  
+    lcd.setCursor(0,1);
+  if(client.available())
+  {
+      wort = client.readStringUntil('/');
+      
+      if(wort == "send")
+      {
+      bet = client.parseInt();
+      lcd.setCursor(8,0);
+      lcd.print("B");
+      lcd.print(bet);
+      lcd.print("   ");
+  
+    wort = client.readStringUntil('/');
+
+      chips = client.parseInt();
+      lcd.setCursor(0,0);
+      lcd.print("CP");
+      lcd.print(chips);
+      lcd.print("  ");    
+    
+    wort = client.readStringUntil('/');
+    highbet = client.parseInt();
+    
+    
+    wort = client.readStringUntil('/');
+    odds = client.parseFloat();
+    lcd.setCursor(0,1);
+    lcd.print(odds*100);
+    lcd.print("%  ");
+ 
+    wort = client.readStringUntil('/');
+    action = client.parseInt();
+    
+    client.flush();
+    
+      
+    
+    
+    Console.println(bet);
+    Console.println(chips);
+    Console.println(highbet);
+    Console.println(odds);
+    Console.println(action);
+      }
   } 
 }
   
